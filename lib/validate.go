3967cbb9177978f4c933d49d0dd2424978930bd4
akhmad-ardi <akhmad.ardi1711@gmail.com>
2025-07-28 09:41:28 +0800

    refactor:validate message

--- lib/validate.go
+++ lib/validate.go
@@ -2,11 +2,33 @@ package lib
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
 
 var Validate = validator.New()
+
+func getJSONFieldName(data interface{}, field string) string {
+	val := reflect.ValueOf(data)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	t := val.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		structField := t.Field(i)
+		if structField.Name == field {
+			jsonTag := structField.Tag.Get("json")
+			jsonName := strings.Split(jsonTag, ",")[0] // ambil nama json sebelum koma
+			if jsonName != "" && jsonName != "-" {
+				return jsonName
+			}
+		}
+	}
+	return field // fallback ke nama field asli
+}
 
 func ValidateWithCustomMessages(data interface{}, messages map[string]map[string]string) map[string]string {
 	err := Validate.Struct(data)
@@ -17,7 +39,7 @@ func ValidateWithCustomMessages(data interface{}, messages map[string]map[string
 	errors := make(map[string]string)
 
 	for _, valErr := range err.(validator.ValidationErrors) {
-		field := valErr.Field()
+		field := getJSONFieldName(data, valErr.Field()) // ambil nama dari tag json
 		tag := valErr.Tag()
 
 		if fieldMessages, ok := messages[field]; ok {
